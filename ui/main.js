13dd0bfc916a52bf1aa711418fd57ecffe511966
Janani-J <30987720+Janani-J@users.noreply.github.com>
2017-09-04 00:38:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,11 +21,25 @@ var button=document.getElementById('counter');
      
      request.open('GET','http://jananinathanee.imad.hasura-app.io/counter',true);
      request.send(null);
-     
-     
-
-
-     
+ };
+ 
+ //Submit name
+ 
+ var nameInput=document.getElementById('name');
+ var name=nameInput.value;
+ var submit=document.getElementById('submit_btn');
+ submit.onclick=function() 
+ {
+  //Capture the list of name
+  
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0;i<names.length;i++)
+  {
+  list+='<li>' + names[i] + '</li>';
+   }
+ var ul=document.getElementById('namelist');
+ ul.innerHTML=list;
  };
 
  
